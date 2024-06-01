b01f2a2d804893c7b806d133236545d0073c85f7
amit <amitmohammad46@gmail.com>
2024-06-01 23:36:53 +0600

    add tab for show fodds by category

--- src/Components/Food/Food.jsx
+++ src/Components/Food/Food.jsx
@@ -1,23 +1,41 @@
 import React, { useEffect, useState } from 'react';
+import { Tabs, Tab, Box } from '@mui/material';
 import FoodItem from './FoodItem';
 
 const Food = () => {
   const allfood = '/src/api/foodapi.json';
 
   const [foods, setFoods] = useState([]);
+  const [category, setCategory] = useState('lunch');
+
   useEffect(() => {
     fetch(allfood)
-    .then(res=> res.json())
-    .then(data => setFoods(data.foods))
-  },[])
-   
+      .then(res => res.json())
+      .then(data => setFoods(data.foods))
+      .catch(error => console.error('Error fetching food data:', error));
+  }, []);
+
+  const handleChange = (event, newValue) => {
+    setCategory(newValue);
+  };
+
   return (
     <div className='py-20'>
       <div className='container mx-auto'>
-        <div className="grid gap-4 grid-cols-2 md:grid-cols-3 ">
-          {
-            foods.filter(fd => fd.category === 'breakfast').slice(0,6).map(fd => (<FoodItem key={fd.id} food={fd}></FoodItem>))
-          }
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tabs value={category} onChange={handleChange} aria-label="food categories" centered>
+            <Tab label="Breakfast" value="breakfast" />
+            <Tab label="Lunch" value="lunch" />
+            <Tab label="Dinner" value="dinner" />
+          </Tabs>
+        </Box>
+        <div className="grid gap-4 grid-cols-2 md:grid-cols-3 mt-4">
+          {foods
+            .filter(fd => fd.category === category) // Filter by selected category
+            .slice(0, 6) // Limit to 6 items
+            .map(fd => (
+              <FoodItem key={fd.id} food={fd} />
+            ))}
         </div>
       </div>
     </div>
--- src/Components/Food/FoodItem.jsx
+++ src/Components/Food/FoodItem.jsx
@@ -4,7 +4,7 @@ const FoodItem = (props) => {
   const {img,title,desc,price} = props.food;
   return (
     <div className='text-center transition-all p-6 bg-white hover:shadow-lg rounded'>
-      <img className='w-full h-64 object-contain' src={img} alt="" />
+      <img className='w-full h-40 md:h-64 object-contain' src={img} alt="" />
       <h2 className='text-2xl font-semibold p'>{title}</h2>
       <p className='text-slate-500'>{desc}</p>
       <span className='text-lg font-semibold text-slate-800'>${price}</span>
